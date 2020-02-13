09d62e64bfd34f965403b1ab1eb600c6b1790ded
endkeyCoder <helplace2019@gmail.com>
2020-02-13 18:52:29 -0300

    correção do retorno de emails

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,15 @@ const routes = Router();
 ''
 routes.post('/sendEmail', async (req, res) => {
     const result = await sendEmail(req.body);
-    console.log('verifique o que aconteceu');
-    console.log(result);
-    res.send({ message: 'server ok', dataEmail: req.body })
+
+    if (result.accepted) {
+        res.status(200).send({ message: 'Email enviado', data: result })
+    } else {
+        res.status(400).send({
+            message: 'Problema ao enviar o email, informe o problema através do whatsapp por favor',
+            data: result
+        })
+    }
 })
 
 routes.get('/', (req, res) => {

4734b7aa805e9c3c4ab5d7fbf3ccc4582e7eae94
endkeyCoder <helplace2019@gmail.com>
2020-02-13 18:36:44 -0300

    configuração porta heroku

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv/config');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
 
